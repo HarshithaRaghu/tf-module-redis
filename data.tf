eb450cb217f3f03dbd4943fd82124a29b39b7e29
Harshitha <harshithadarshan95@gmail.com>
2023-02-22 10:17:34 +0530

    Autopush

--- data.tf
+++ data.tf
@@ -2,7 +2,7 @@
 data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
-        bucket = "b52-terraform-state-bucket"
+        bucket = "b52-terraform-state-bucket1"
         key    = "vpc/${var.ENV}/terraform.tfstate"
         region = "us-east-1"
   }
